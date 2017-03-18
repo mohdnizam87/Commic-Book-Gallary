df3ad5a28e2c5e63fe1a4ffe0ae2e5e1a3f11166
MohdNizam <mohd.nizam.ghafar@gmail.com>
2017-03-18 21:58:11 +0800

    Added Comic book controller

---
+++ ComicBookGallary/Controllers/ComicBooksController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallary.Controllers
+{
+    //inherit base class controller
+    public class ComicBooksController: Controller
+    {
+
+    }
+}
